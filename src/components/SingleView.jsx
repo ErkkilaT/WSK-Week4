6c7ae89639078c3ccd988c797bc871f8998bf916
ErkkilaT <teemu.erkkila@gmail.com>
2025-04-08 09:22:14 +0300

    state-management done

--- src/components/SingleView.jsx
+++ src/components/SingleView.jsx
@@ -18,11 +18,6 @@ const SingleView = (props) => {
         </dialog>
       )}
     </>
-    // TODO: Add JSX for displaying a mediafile here
-    // - use e.g. a <dialog> element for creating a modal
-    // - use item prop to render the media item details
-    // - use img tag for displaying images
-    // - use video tag for displaying videos
   );
 };
 export default SingleView;
